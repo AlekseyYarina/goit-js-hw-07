66227f515e6e93b13d51660e2cea7333d1e49087
AlekseyYarina <aleksey.yarina@gmail.com>
2023-10-16 14:13:12 +0300

    add task-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const imgOfGallary = galleryItems.map(({ preview, original, description }) => {
+  return `<li class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" /> </a></li>`;
+});
+gallery.innerHTML = imgOfGallary.join("");
+
+gallery.addEventListener("click", getOriginalImg);
+
+let lightboxInstance;
+function getOriginalImg(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  const imgUrl = e.target.getAttribute("data-source");
+  lightboxInstance = basicLightbox.create(
+    `<img src="${imgUrl}" width='1280' heigth='853'>`
+  );
+  lightboxInstance.show();
+
+  window.addEventListener("keydown", onCloseKeydown);
+  
+  function onCloseKeydown(e) {
+    if (e.code === "Escape") {
+      lightboxInstance.close();
+      window.removeEventListener("keydown", onCloseKeydown);
+    }
+  };
+}
+  
+  
+  
+  
+  
+  
